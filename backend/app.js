fead85692a7491325bfb5ab29a37e3b4b6cad365
Jaeseok <jscadman@yonsei.ac.kr>
2020-05-03 13:47:34 +0900

    1.0.3

--- backend/app.js
+++ backend/app.js
@@ -3,22 +3,39 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const db = require('./models');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
 
+const prod = process.env.NODE_ENV === 'production';
 const passport = require('passport');
 const cookie = require('cookie-parser');
 const session = require('express-session');
 
+dotenv.config();
 db.sequelize.sync({force : false});
 const passportConfig = require('./passport');
 const userRouter = require('./routes/user');
 const customerRouter = require('./routes/customer');
 
 
-app.use(morgan('dev'));
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
+if(prod){
+    app.use(helmet());
+    app.use(hpp());
+    app.use(morgan('combined'));
+    app.use(cors({
+        origin: 'http://wdfo.co.kr',
+        credentials: true,
+    }));
+}else{
+    app.use(morgan('dev'));
+    app.use(cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    }));
+}
+
+
 passportConfig();
 app.use(cookie('coockiesecret'));
 
@@ -33,10 +50,11 @@ app.get('/', (req, res) => {
 app.use(session({
     resave: false,
     saveUninitialized: false,
-    secret: 'cookiesecret',
+    secret: 'process.env.COOKIE_SECRET',
     cookie: {
         httpOnly: true,
         secure: false,
+        domain: prod && '.wdfo.co.kr',
     }
 }));
 app.use(passport.initialize());
@@ -45,6 +63,6 @@ app.use(passport.session());
 app.use('/user', userRouter);
 app.use('/customer', customerRouter);
 
-app.listen(3085, () => {
+app.listen(prod? process.env.PORT : 3085, () => {
     console.log(`백엔드 서버 ${3085}번 포트에서 작동 중`)
 });
